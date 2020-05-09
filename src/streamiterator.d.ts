cc7c9f8f67173b8c1878001a6d667ed7b6124cb7
Vadzim Zieńka <v@vadzim.info>
2020-05-09 16:28:51 +0300

    remove extra type definitions

--- src/streamiterator.d.ts
+++
@@ -1,9 +0,0 @@
-export async function* streamiterator<T>(stream: Iterable<PromiseLike<T> | T>): AsyncIterable<T>
-export async function* streamiterator<T>(stream: AsyncIterable<T>): AsyncIterable<T>
-
-export async function* streamiterator(stream: PromiseLike<Body>): AsyncIterable<string | Buffer | Uint8Array>
-export async function* streamiterator(stream: Body): AsyncIterable<string | Buffer | Uint8Array>
-
-export async function* streamiterator(stream: Blob): AsyncIterable<string | Buffer | Uint8Array>
-export async function* streamiterator<T>(stream: ReadableStream<T>): AsyncIterable<T>
-export async function* streamiterator<T>(stream: ReadableStreamDefaultReader<T>): AsyncIterable<T>
